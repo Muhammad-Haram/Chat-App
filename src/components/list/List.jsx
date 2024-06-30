c8ed789d3e8833719b6488e56ffee3cd6ca31dc3
Muhammad Haram <haramh643@gmail.com>
2024-06-30 13:06:43 +0500

    commit

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,10 +1,12 @@
 import "./list.css"
 import React from 'react'
+import UserInfo from "./userInfo/UserInfo"
 
 const List = () => {
     return (
         <div>
             List
+            <UserInfo/>
         </div>
     )
 }
---
+++ src/components/list/userInfo/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div>
+      userinfo
+    </div>
+  )
+}
+
+export default UserInfo
