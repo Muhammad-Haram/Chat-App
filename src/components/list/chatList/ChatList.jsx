56a658295dabfe9235d8abb81f0a51c341c9d2a5
Muhammad Haram <haramh643@gmail.com>
2024-07-04 16:01:10 +0500

    chat list styling

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chatList.css'
 
 const ChatList = () => {
+
+  const [addMore, setAddMore] = useState(false);
+
   return (
     <div className='chatList'>
       <div className="search">
-
         <div className="searchBar">
           <img src="/public/search.png" alt="" />
           <input type="text" placeholder='Search' />
         </div>
-        <img src="/public/plus.png" alt="" className='add-img' />
+        <img src={addMore ? '/public/minus.png' : '/public/plus.png'} alt="" className='add-img'
+          onClick={() => setAddMore((e) => !e)} />
       </div>
+
+      <div className='item'>
+        <img src="/public/avatar.png" alt="" />
+        <div className='texts'>
+          <span>Jane Doe</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className='item'>
+        <img src="/public/avatar.png" alt="" />
+        <div className='texts'>
+          <span>Jane Doe</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className='item'>
+        <img src="/public/avatar.png" alt="" />
+        <div className='texts'>
+          <span>Jane Doe</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className='item'>
+        <img src="/public/avatar.png" alt="" />
+        <div className='texts'>
+          <span>Jane Doe</span>
+          <p>hello</p>
+        </div>
+      </div>
+
     </div>
   )
 }
