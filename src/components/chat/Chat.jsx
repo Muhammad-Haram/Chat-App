398855948e698391c0e27edffe3759ae186f57f3
Muhammad Haram <haramh643@gmail.com>
2024-07-04 22:09:14 +0500

    install package and create botton bar in chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -18,8 +18,24 @@ const Chat = () => {
           <img src="/public/info.png" alt="" />
         </div>
       </div>
+
       <div className="center"></div>
-      <div className="bottom"></div>
+      
+      <div className="bottom">
+
+        <div className="icons">
+          <img src="/public/img.png" alt="" />
+          <img src="/public/camera.png" alt="" />
+          <img src="/public/mic.png" alt="" />
+        </div>
+
+        <input type="text" placeholder="Type a Message..." />
+        <div className="emoji">
+          <img src="/public/emoji.png" alt="emoji" />
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+
     </div>
   )
 }
