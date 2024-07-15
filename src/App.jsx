3609b3620a34bcc5afb8a7b2bb9858f1e75e3c9f
Muhammad Haram <haramh643@gmail.com>
2024-07-16 02:01:45 +0500

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { auth } from "./lib/firebase";
 const App = () => {
 
   const user = false;
+
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
       console.log(user)
