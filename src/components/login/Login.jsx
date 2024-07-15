e9e57cb6af61490a46deb56d65257bddc420c78a
Muhammad Haram <haramh643@gmail.com>
2024-07-16 01:23:46 +0500

    add loading and sign in

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { auth, db } from '../../lib/firebase'
 import { doc, setDoc } from 'firebase/firestore'
+import upload from '../../lib/upload'
 
 const Login = () => {
-    const [avatar, setAvatar] = useState({ file: null, url: '' })
+    const [avatar, setAvatar] = useState({ file: null, url: '' });
+
+    const [loading, setLoading] = useState(false);
 
     const handleAvatar = (e) => {
         if (e.target.files[0]) {
@@ -15,23 +18,39 @@ const Login = () => {
                 url: URL.createObjectURL(e.target.files[0])
             })
         }
-    }
+    };
 
-    const handleLogin = (ele) => {
+    const handleLogin = async (ele) => {
         ele.preventDefault();
-        toast.warn('hello');
-    }
+        setLoading(true);
+
+        const formData = new FormData(ele.target);
+        const { email, password } = Object.fromEntries(formData);
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const handleRegister = async (ele) => {
         ele.preventDefault();
+        setLoading(true);
         const formData = new FormData(ele.target);
         const { username, email, password } = Object.fromEntries(formData);
 
         try {
             const response = await createUserWithEmailAndPassword(auth, email, password);
 
+            const imgUrl = await upload(avatar.file);
+
             await setDoc(doc(db, 'users', response.user.uid), {
                 id: response.user.uid,
+                avatar: imgUrl,
                 username: username,
                 email: email,
                 password: password,
@@ -47,8 +66,10 @@ const Login = () => {
         } catch (error) {
             console.log(error)
             toast.error(error.message);
+        } finally {
+            setLoading(false);
         }
-    }
+    };
 
     return (
         <div className='login'>
@@ -58,7 +79,7 @@ const Login = () => {
                 <form onSubmit={handleLogin}>
                     <input type="text" placeholder='Your Email Address' name='email' />
                     <input type="password" placeholder='Your Password' name='password' />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{loading ? "loading" : "Sign In"}</button>
                 </form>
             </div>
 
@@ -72,7 +93,7 @@ const Login = () => {
                     <input type="text" placeholder='Username' name='username' />
                     <input type="text" placeholder='Your Email Address' name='email' />
                     <input type="password" placeholder='Your Password' name='password' />
-                    <button>Sign Up</button>
+                    <button disabled={loading}>{loading ? "loading" : "Sign Up"}</button>
                 </form>
             </div>
 
